d27bc5a62fed9a9e0d7083ca19870de01b411716
vleadadmin <admin@vlabs.ac.in>
2025-04-22 09:24:33 +0000

    https://virtual-labs.github.io/exp-precom-proced-coep click on the link to test your code.

---
+++ simulation/src/stdJson.js
@@ -0,0 +1,150 @@
+var quizData = {
+		  "questions": [
+			    {
+			    	"QuizTitle":"Refer Page No. 129 of the report which depicts the wiring diagram of the Boiler and Heat Exchanger plant operated through PLC. Answer the following questions to verify the functioning of the components.",
+			      "question": "What is the purpose of Isolators IS 5 to 8?",
+			      "options": [
+			        "To isolate the noise from the sensor signal",
+			        "To duplicate the input signal",
+			        "To isolate the power fluctuations",
+			        "To connect to PLC"
+			      ],
+			      "correct_option": "To isolate the noise from the sensor signal"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 129 of the report which depicts the wiring diagram of the Boiler and Heat Exchanger plant operated through PLC. Answer the following questions to verify the functioning of the components.",
+					   
+			      "question": "How do you test that the isolator is not functional?",
+			      "options": [
+			        "By checking the power supply at terminals L+ and L-",
+			        "By checking the current at terminals ISS OP-1 and ISS OP-2",
+			        "By looking at the LED indicator on front panel",
+			        "All test points as mentioned."
+			      ],
+			      "correct_option": "All test points as mentioned."
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 129 of the report which depicts the wiring diagram of the Boiler and Heat Exchanger plant operated through PLC. Answer the following questions to verify the functioning of the components.",
+					   
+			      "question": "What will happen if the power supply to the Isolator is disconnected either L+ or L-?",
+			      "options": [
+			        "Analog Input card M2 channels will not receive any signal",
+			        "Only Analog Input card M2 channels will receive any signal",
+			        "The transmitters at input side will go in 'power off' mode",
+			        "The system will behave as usual"
+			      ],
+			      "correct_option": "Analog Input card M2 channels will not receive any signal"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 131 of the report which depicts the wiring diagram of the Boiler and Heat Exchanger plant operated through PLC. Answer the following questions to verify the functioning of the components.",
+					   
+			      "question": "The purpose of RL1 to RL4 is to",
+			      "options": [
+			        "Multiplication of signal",
+			        "Isolation to Digital output card",
+			        "Impedance matching",
+			        "None mentioned"
+			      ],
+			      "correct_option": "Isolation to Digital output card"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 131 of the report which depicts the wiring diagram of the Boiler and Heat Exchanger plant operated through PLC. Answer the following questions to verify the functioning of the components.",
+					
+			      "question": "What is the coil rating of the relays used on relay card?",
+			      "options": [
+			        "115 VAC",
+			        "230 VAC",
+			        "24 VDC",
+			        "Universal power supply"
+			      ],
+			      "correct_option": "24 VDC"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 131 of the report which depicts the wiring diagram of the Boiler and Heat Exchanger plant operated through PLC. Answer the following questions to verify the functioning of the components.",
+					
+			      "question": "What will be the signal level at terminal no. TBR6 1 and 2?",
+			      "options": [
+			        "24 VDC",
+			        "115 VAC",
+			        "4~20 mA",
+			        "1~5 VDC"
+			      ],
+			      "correct_option": "24 VDC"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 179 and allied pages of the report which depicts the ladder diagram of the Evaporator plant operated through PLC. Answer the following questions to verify the functioning of the components as part of pre-commissioning activity.",
+					
+			      "question": "What is the meaning of rung 5 which expects input from Remote/Local?",
+			      "options": [
+			        "As per automation philosophy this plant can be operated by an operator locally or from remote destination",
+			        "The selector switch is used for passing control to remote terminal if it is put on Local",
+			        "The switch will activate a relay",
+			        "The facility is non-functional"
+			      ],
+			      "correct_option": "As per automation philosophy this plant can be operated by an operator locally or from remote destination"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 179 and allied pages of the report which depicts the ladder diagram of the Evaporator plant operated through PLC. Answer the following questions to verify the functioning of the components as part of pre-commissioning activity.",
+					
+			      "question": "What is the purpose of scaling function?",
+			      "options": [
+			        "Re-calibrate the flow transmitter",
+			        "Fit the value of FT in normalized form",
+			        "Configurability to user",
+			        "None mentioned"
+			      ],
+			      "correct_option": "Fit the value of FT in normalized form"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 179 and allied pages of the report which depicts the ladder diagram of the Evaporator plant operated through PLC. Answer the following questions to verify the functioning of the components as part of pre-commissioning activity.",
+					
+			      "question": "On page 285 why MOV instruction is used for many parameters?",
+			      "options": [
+			        "To display the readings on external devices",
+			        "To transfer the data before it is saved",
+			        "To create a copy of the data",
+			        "None mentioned"
+			      ],
+			      "correct_option": "To transfer the data before it is saved"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 179 and allied pages of the report which depicts the ladder diagram of the Evaporator plant operated through PLC. Answer the following questions to verify the functioning of the components as part of pre-commissioning activity.",
+					
+			      "question": "For pre-commissioning activities which part of the report is important to scan first?",
+			      "options": [
+			        "Process Description",
+			        "Control strategies",
+			        "Control of plant remotely",
+			        "Control of plant using Virtual PLC"
+			      ],
+			      "correct_option": "Control strategies"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 179 and allied pages of the report which depicts the ladder diagram of the Evaporator plant operated through PLC. Answer the following questions to verify the functioning of the components as part of pre-commissioning activity.",
+					
+			      "question": "The ladder program listed in Appendix F page no. 269 onwards, which portion of the program needs to be checked as pre-commissioning activity?",
+			      "options": [
+			        "Initialization",
+			        "Running",
+			        "Shut down",
+			        "All mentioned"
+			      ],
+			      "correct_option": "All mentioned"
+			    },
+			    {
+			    	"QuizTitle":"Refer Page No. 179 and allied pages of the report which depicts the ladder diagram of the Evaporator plant operated through PLC. Answer the following questions to verify the functioning of the components as part of pre-commissioning activity.",
+					
+			      "question": "As part of pre-commissioning activity how will you test functional aspect of running of a plant without compromising safety?",
+			      "options": [
+			        "By observing the mimic as shown on page 23",
+			        "By understanding working on three element drum level controller as shown on page no.28",
+			        "Swell and shrink on page no. 31",
+			        "Studying two element drum level controller on page no. 33"
+			      ],
+			      "correct_option": "By observing the mimic as shown on page 23"
+			    }
+			  ]
+			}
+
+    console.log(quizData);
+ 
